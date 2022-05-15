914a8f20c95e002076887f0c607adbf90aa360d3
Eduardo Sato <eddsato@Eduardos-MacBook-Pro.local>
2022-05-14 23:35:05 -0300

    populate the database before application starts

--- codelabs-room/app/src/main/java/com/eddsato/codelabsroom/WordApplication.kt
+++ codelabs-room/app/src/main/java/com/eddsato/codelabsroom/WordApplication.kt
@@ -1,8 +1,11 @@
 package com.eddsato.codelabsroom
 
 import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class WordApplication : Application() {
-    val database by lazy { WordRoomDatabase.getDatabase(this) }
+    val applicationScope = CoroutineScope(SupervisorJob())
+    val database by lazy { WordRoomDatabase.getDatabase(this, applicationScope) }
     val repository by lazy { WordRepository(database.wordDao()) }
 }
